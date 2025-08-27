27e3121520e9d0726d81d4a6989604f2a0101a5d
Micaela Camerini <micaelacamerini19@gmail.com>
2025-08-26 22:24:34 -0300

    testeo postman 1

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const ProductManager = require('../managers/product.manager');
-// Creamos un router específico para productos
+
 const router = express.Router();
 const productManager = new ProductManager();
 
-// GET / → listar todos los productos
+
 router.get('/', async (req, res) => {
     const products = await productManager.getProducts();
     res.json(products);
 });
 
-// GET /:pid → obtener producto por ID
+
 router.get('/:pid', async (req, res) => {
     const product = await productManager.getProductById(req.params.pid);
     if (!product) return res.status(404).json({ error: 'Producto no encontrado' });
     res.json(product);
 });
 
-// POST / → agregar producto
+
 router.post('/', async (req, res) => {
     const newProduct = await productManager.addProduct(req.body);
     res.status(201).json(newProduct);
 });
 
-// PUT /:pid → actualizar producto
+
 router.put('/:pid', async (req, res) => {
     const updatedProduct = await productManager.updateProduct(req.params.pid, req.body);
     if (!updatedProduct) return res.status(404).json({ error: 'Producto no encontrado' });
     res.json(updatedProduct);
 });
 
-// DELETE /:pid → eliminar producto
+
 router.delete('/:pid', async (req, res) => {
     await productManager.deleteProduct(req.params.pid);
     res.json({ message: 'Producto eliminado' });
